514969c8f342a79bbfb568559bd2910a0d5180c7
HHP <gusvy09@naver.com>
2024-01-09 11:46:49 +0900

    first

---
+++ MyServer_240102/MyServer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace MyServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PortText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("돋움", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(119, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(584, 221);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(495, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(243, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "server off";
+            // 
+            // PortText
+            // 
+            this.PortText.Location = new System.Drawing.Point(10, 41);
+            this.PortText.Margin = new System.Windows.Forms.Padding(2);
+            this.PortText.Name = "PortText";
+            this.PortText.Size = new System.Drawing.Size(105, 21);
+            this.PortText.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("돋움", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(8, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("돋움", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(8, 303);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(538, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "엔터를 누르면 채팅을 전송하고 Save 버튼을 누르면 client db에 저장됩니다.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PortText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PortText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyServer_240102/MyServer/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace MyServer
+{
+    public partial class Form1 : Form
+    {
+        //ip, port 할당 (local ip, port번호는 임의)
+        private string ip = "127.0.0.1";
+        private int port = -1;
+
+        // 데이터를 읽고 쓰기 위한 스트림리더,라이터
+        bool connected = false;
+        StreamReader StreamReader1;
+        StreamWriter StreamWriter1;
+
+        // on, off를 위해 전역 변수로 선언
+        Thread thread1;
+        TcpListener tcpListener1;
+        TcpClient tcpClient1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 연결 버튼
+        {
+            if(button1.Text == "Start") // 서버 실행전 On으로 상태 변경
+            {
+                Boolean bPort = true;
+                bPort = int.TryParse(PortText.Text, out port);
+                if (bPort && port>0) // 포트 번호가 제대로 입력되어 있는 경우
+                {
+                    button1.Text = "Stop"; // 실행되는 상태
+                    label1.Text = "server on";
+                    writeRichText("서버 ON");
+                    thread1 = new Thread(connect);
+                    thread1.IsBackground = true;
+                    thread1.Start();
+                }
+                else
+                {
+                    writeRichText("port번호를 다시 확인하세요");
+                }
+            }
+            else // 서버 실행후 Off로 상태 변경
+            {
+                button1.Text = "Start"; 
+                label1.Text = "server off";
+                writeRichText("서버 OFF");
+                if (tcpClient1 == null) // 연결된 클라이언트 없이 끌때
+                {
+                    tcpListener1.Server.Close();
+                    thread1 = null;
+                    GC.Collect();
+                }
+                else if (tcpClient1.Connected) // 연결된 클라이언트가 있으면
+                {
+                    // 메모리 해제 및 연결 끊기
+                    StreamWriter1.WriteLine("[server]:ServerIsClose");
+                    StreamReader1.Close();
+                    StreamWriter1.Close();
+                    tcpListener1.Server.Close();
+                    connected = false; // while문 탈출을 위해서
+                    tcpClient1 = null; 
+                    thread1 = null;
+                }
+                
+            }
+            
+        }
+        // tcpclient를 이용하여 stream 연결하는 메서드
+        private void connect_Stream(TcpClient tcp)
+        {
+            StreamReader1 = new StreamReader(tcp.GetStream()); // 읽기 스트림 연결
+            StreamWriter1 = new StreamWriter(tcp.GetStream()); // 쓰기 스트림 연결
+            StreamWriter1.AutoFlush = true; // 쓰기 버퍼 자동으로 처리
+        }
+        private void connect() // 연결
+        {
+            try
+            {
+                tcpListener1 = new TcpListener(IPAddress.Parse(ip), port); //ip와 port번호 이용하여 서버 open
+                tcpListener1.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                tcpListener1.Start();
+                writeRichText("서버에서 클라이언트 기다리는중");
+                
+                tcpClient1 = tcpListener1.AcceptTcpClient(); // client가 연결될 때 까지 대기
+
+                ReceiveFromClient(tcpClient1);
+            }
+            catch {  }
+            finally
+            {
+            }
+        }
+
+        private void writeRichText(string msg) // richtextbox 업데이트
+        {
+            richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText(msg + "\r\n"); }); // 리치텍스트에 한 줄 입력
+            richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.ScrollToCaret(); }); // 스크롤 맨 아래로
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) // 텍스트박스에서 엔터 누르면 채팅전달
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                string data = textBox1.Text;
+                Send_Data("[chat]:"+data);
+            }
+        }
+        private void Send_Data(string data) // 클라이언트에 데이터를 전송하는 메서드
+        {
+            if ( tcpClient1 == null || !tcpClient1.Connected) // 텍스트가 비어있거나, 클라이언트에 연결이 되어있지 않다면
+            {
+                writeRichText("연결된 클라이언트가 없습니다.");
+                textBox1.Text = ""; // 초기화
+            }
+            else if (textBox1.Text.Trim() == "")
+            {
+                writeRichText("보낼 메세지를 입력해주세요");
+                textBox1.Text = ""; // 초기화
+            }
+            else
+            {
+                StreamWriter1.WriteLine(data);
+                textBox1.Text = ""; // 송신 후 초기화
+            }
+        }
+        private void ReceiveFromClient(TcpClient tcpClient)
+        {
+            connect_Stream(tcpClient); // client와 연결 후 stream 연결
+            connected = true;
+            writeRichText("클라이언트 연결 완료");
+            while (connected) // clinet와 연결이 되어있는 동안
+            {
+                string receiveData1 = StreamReader1.ReadLine(); // 데이터 읽기
+                if(receiveData1 == "disconnect_server")
+                {
+                    StreamWriter1.WriteLine("[server]:ServerIsClose");
+                    connected = false;
+                    StreamReader1.Close();
+                    StreamWriter1.Close();
+                    tcpListener1.Server.Close();
+                    tcpClient1 = null;
+                    thread1 = new Thread(connect);
+                    thread1.IsBackground = true;
+                    thread1.Start();
+                    break;
+                }
+                string[] type = receiveData1.Split(':');  
+                if (type[0] == "chat")
+                {
+                    writeRichText(type[1]); // 읽어온 데이터 richtext에 추가 (채팅)
+                }
+                else if (type[0] == "data")
+                {
+                    // data 처리 및 return
+                    return_data(type[1]);
+                }
+            }
+        }
+        // data 처리 및 리턴 (정상데이터는 OK붙여서 리턴)
+        private void return_data(string data)
+        {
+            writeRichText(data);
+            // 받은 데이터를 ; 기준으로 나눔 (현재 FLAG 컬럼의 값이 없기 때문에 substring으로 잘라서 구분, 추후에 값이 생기면 substring하지 않아도 됨)
+            string[] check = data.Substring(0,data.Length).Split(';');
+            bool flag = true;
+            // 나눈 데이터가 정상(빈칸이 없음)인지 아닌지 구별
+            for(int i = 0; i < check.Length-1; i++)
+            {
+                if(check[i] == "" || check[i] == null)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            // 정상데이터는 OK를 붙여서 반환
+            if (flag)
+            {
+                StreamWriter1.WriteLine("test:" + data + "OK");
+            }
+            // 빈 문자열 or null이 포함되어 있으면 그냥 반환
+            else
+            {
+                StreamWriter1.WriteLine("test:" + data);
+                flag = true;
+            }
+        }
+    }
+}
